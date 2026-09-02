f43ce91d12c5f65bcc14302c8d55ce3b5b8dcd82
Kriti Gupta <156417457+Kriti1908@users.noreply.github.com>
2026-09-03 00:54:19 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> freq(26,0);
+        for(char i: s) {
+            freq[i-'a']++;
+        }
+        for(char i : t) {
+            if(freq[i-'a'] == 0) return false;
+            freq[i-'a']--;
+        }
+        for(int i=0; i<26; i++) {
+            if(freq[i] != 0) return false;
+        }
+        return true;
+    }
+};
